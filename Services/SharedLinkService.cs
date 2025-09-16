f98e30072e0b299a74f21ce5054559385de36726
Alejandro <riveraconv@gmail.com>
2025-09-16 12:43:13 +0200

    Share class and listener event defined

---
+++ Services/SharedLinkService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silksong.Services
+{
+    public class SharedLinkService :  ISharedLinkService
+    {
+        public event Action<string>? LinkReceived;
+
+        public void ReceivedSharedLink(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url)) return;
+
+            LinkReceived?.Invoke(url);
+        }
+    }
+}
